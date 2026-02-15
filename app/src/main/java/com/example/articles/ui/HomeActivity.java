a29204c6d2347752ab5249f56d2c754b6bf047b2
mouadams <mouadtahiri1000@gmail.com>
2026-02-15 21:29:38 +0000

    fix some bugs

--- app/src/main/java/com/example/articles/ui/HomeActivity.java
+++ app/src/main/java/com/example/articles/ui/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.articles.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.Request;
 import com.android.volley.toolbox.StringRequest;
+import com.bumptech.glide.Glide;
 import com.example.articles.models.Article;
 import com.example.articles.R;
 import com.example.articles.adapters.ArticleAdapter;
@@ -48,5 +52,32 @@ public class HomeActivity extends AppCompatActivity {
         );
 
         VolleyHelper.getInstance(this).addToRequestQueue(request);
+    }
+
+    public static class DetailActivity extends AppCompatActivity {
+
+        ImageView detailImg;
+        TextView detailTitle, detailBody;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_detail);
+
+            detailImg = findViewById(R.id.detailImg);
+            detailTitle = findViewById(R.id.detailTitle);
+            detailBody = findViewById(R.id.detailBody);
+
+            Intent intent = getIntent();
+
+            detailTitle.setText(intent.getStringExtra("title"));
+            detailBody.setText(intent.getStringExtra("body"));
+
+            String imageUrl = intent.getStringExtra("image");
+
+            Glide.with(this)
+                    .load(imageUrl)
+                    .into(detailImg);
+        }
     }
 }
